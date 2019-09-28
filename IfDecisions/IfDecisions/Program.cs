f38e39db1a7c36069469fb122813df2025c015f3
Billy Kim <billyhkim.dev@gmail.com>
2019-09-28 16:00:26 -0700

    IfDecisions adding conditionals

---
+++ IfDecisions/IfDecisions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace IfDecisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Billy's Big Giveaway");
+            Console.Write("Choose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = "";
+
+            if (userValue == "1")
+                message = "You won a new car!";
+            else if (userValue == "2")
+                message = "You won a new boat!";
+            else if (userValue == "3")
+                message = "You won a new dog!";
+            else
+            {
+                message = "Sorry, not a choice! ";
+                message += "You Lose.";
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("Billy's Big Giveaway");
+            Console.Write("Choose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            // Ternary work like JS
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            Console.WriteLine("You won a {0}, {1}.", message, "Billy");
+            Console.ReadLine();
+        }
+    }
+}
