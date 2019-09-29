0e064f8354d2dd3562b93a1f128bf1434e53f37d
Billy Kim <billyhkim.dev@gmail.com>
2019-09-29 11:25:39 -0700

    adding variable types exercise

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // Like TypeScript
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+            */
+        }
+    }
+}
