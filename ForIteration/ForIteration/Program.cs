06c0b7804386e4583ad070963962a68bb7951d85
Billy Kim <billyhkim.dev@gmail.com>
2019-09-29 21:13:15 -0700

    foriteration initiate debugging

---
+++ ForIteration/ForIteration/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 7)
+                {
+                    Console.WriteLine("Found 7!");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
